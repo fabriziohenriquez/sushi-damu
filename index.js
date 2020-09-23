d9ab61f465296fa96921f62629699f75750d1bdc
Fabri HZ <fabri.hz@hotmail.com>
2020-09-22 21:32:17 -0500

    Move newsletter out of footer into home page, no special images on mobile

--- index.js
+++ index.js
@@ -22,8 +22,4 @@ $("#hours-link-nav").click(function() {
 // Mobile
 if ($(window).width() < 1000) {
   $("#map").attr("width", "350");
-  $("#special-1").attr("src", "images/special-img-1.jpg");
-  $("#special-2").attr("src", "images/special-img-2.jpg");
-  $("#special-3").attr("src", "images/special-img-3.jpg");
-  $("#special-4").attr("src", "images/special-img-4.jpg");
 }
